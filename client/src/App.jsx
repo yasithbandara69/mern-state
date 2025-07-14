f16770a702bdecf75c6d679a7b0778aa4a6f25b7
Yasith bandara weerasekara <it23645370@my.sliit.lk>
2025-07-14 15:31:31 +0530

    add react pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,23 @@
-import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import About from "./pages/About";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="text-red-500">
-      <p>Hello world</p>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/signin" element={<SignIn />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <div>
+        <h1 className="font-bold">
+          <span className="text-slate-500">CodingCleverly</span>
+          <span className="text-slate-600">Estate</span>
+        </h1>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
